e424bd582ff2b3929c95aaf459df7e48486bd1bb
murilodestefani <murilo_destefani@hotmail.com>
2024-06-12 21:28:37 -0400

    Feature: verification of ip has voted

--- server.js
+++ server.js
@@ -2,26 +2,27 @@ import {
   loadPackageDefinition,
   Server,
   ServerCredentials,
-} from "@grpc/grpc-js";
-import { loadSync } from "@grpc/proto-loader";
+} from '@grpc/grpc-js';
+import { loadSync } from '@grpc/proto-loader';
 
-const votingDefs = loadSync("./voting.proto");
+const votingDefs = loadSync('./voting.proto');
 const votingProto = loadPackageDefinition(votingDefs).VotingService;
 
 const votes = [
-  { candidateId: 1, name: "João da Silva", votes: 0 },
-  { candidateId: 2, name: "Fernando Santos", votes: 0 },
-  { candidateId: 3, name: "Carlos Oliveira", votes: 0 },
-  { candidateId: 4, name: "Ricardo Almeida", votes: 0 },
-  { candidateId: 5, name: "Felipe Martins", votes: 0 },
-  { candidateId: 6, name: "Dilma Ferreira", votes: 0 },
-  { candidateId: 7, name: "Mariana Costa", votes: 0 },
-  { candidateId: 8, name: "Fernanda Souza", votes: 0 },
-  { candidateId: 9, name: "Patrícia Lima", votes: 0 },
-  { candidateId: 10, name: "Renata Barbosa", votes: 0 },
+  { candidateId: 1, name: 'João da Silva', votes: 0 },
+  { candidateId: 2, name: 'Fernando Santos', votes: 0 },
+  { candidateId: 3, name: 'Carlos Oliveira', votes: 0 },
+  { candidateId: 4, name: 'Ricardo Almeida', votes: 0 },
+  { candidateId: 5, name: 'Felipe Martins', votes: 0 },
+  { candidateId: 6, name: 'Dilma Ferreira', votes: 0 },
+  { candidateId: 7, name: 'Mariana Costa', votes: 0 },
+  { candidateId: 8, name: 'Fernanda Souza', votes: 0 },
+  { candidateId: 9, name: 'Patrícia Lima', votes: 0 },
+  { candidateId: 10, name: 'Renata Barbosa', votes: 0 },
 ];
 
 const votingServer = new Server();
+const ipVoted = [];
 
 votingServer.addService(votingProto.service, {
   Vote: (call, callback) => {
@@ -30,13 +31,29 @@ votingServer.addService(votingProto.service, {
       (candidate) => candidate.candidateId === candidateId
     );
 
-    if (findCandidate) {
+    const ip = call.getPeer().split(':');
+    console.log(ip[0]);
+    console.log(ipVoted);
+    let message = '';
+    let name = '';
+
+    const isVoted = ipVoted.find((ipVoted) =>
+      ip[0] === ipVoted ? true : false
+    );
+
+    if (findCandidate && !isVoted) {
       findCandidate.votes++;
+      ipVoted.push(ip[0]);
+      message = '📥 Voto registrado com sucesso';
+      name = findCandidate.name;
+    } else {
+      message = `Cliente com ${ip} já votou!`;
+      votingServer.forceShutdown();
     }
 
     callback(null, {
-      message: "📥 Voto registrado com sucesso",
-      name: findCandidate.name,
+      message,
+      name,
     });
   },
 
@@ -51,7 +68,7 @@ votingServer.addService(votingProto.service, {
   },
 });
 
-const serverAddress = "0.0.0.0:5051";
+const serverAddress = '0.0.0.0:5051';
 votingServer.bindAsync(
   serverAddress,
   ServerCredentials.createInsecure(),
